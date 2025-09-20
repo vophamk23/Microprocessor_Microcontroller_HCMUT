3751e52741f3e5926e8b049b1ea9bebfba167474
vophamk23 <congvolv1@gmail.com>
2025-09-20 21:08:21 +0700

    Lab Microcontroller Hk251

--- STM32 LAB 1 - LED ANIMATION/Ex2_TrafficLight/STM32CubeIDE project/Debug/Core/Startup/subdir.mk
+++ STM32 LAB 1 - LED ANIMATION/Ex2_TrafficLight/STM32CubeIDE project/Debug/Core/Startup/subdir.mk
@@ -5,13 +5,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 S_SRCS += \
-../Core/Startup/startup_stm32f103c6tx.s 
+../Core/Startup/startup_stm32f103c6ux.s 
 
 OBJS += \
-./Core/Startup/startup_stm32f103c6tx.o 
+./Core/Startup/startup_stm32f103c6ux.o 
 
 S_DEPS += \
-./Core/Startup/startup_stm32f103c6tx.d 
+./Core/Startup/startup_stm32f103c6ux.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
